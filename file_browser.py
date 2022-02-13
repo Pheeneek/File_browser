316776efab017b04f5e94b3b2568c0799b1fbe31
PheeNeek <Ustas184@gmaill.com>
2022-02-13 12:10:06 +0300

    Переделал, чтобы в url был только хэш, разделил класс на 2 функции

--- file_browser.py
+++ file_browser.py
@@ -6,48 +6,42 @@ from dotenv import load_dotenv
 
 
 class Search:
-    dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
-    if os.path.exists(dotenv_path):
-        load_dotenv(dotenv_path)
-    path = os.environ['VIDEO']
-
-    def __init__(self, path_name=path):
+    def __init__(self):
         self.file_extensions = ['.avi', '.mkv', '.mp4', '.vob', '.mpg', '.flv', '.asx', '.exe']
-        self.result_list = {}
+        dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+        if os.path.exists(dotenv_path):
+            load_dotenv(dotenv_path)
+        self.path_name = os.environ['VIDEO']
         app = Flask(__name__)
-        for item in os.walk(path_name):
-            self.result_list[item[0]] = ["\\" + x for x in item[1]], \
-                                        [y for y in item[2] if y[-4:] in self.file_extensions]
-
-        @app.route("/<hashed>")
-        def answer(hashed):
-            for key in self.result_list:
-                previous_dir = 0 if key.replace("/", "\\") == os.environ["VIDEO"]\
-                    else os.path.dirname(key.replace("/", "\\"))
-                if hashlib.md5(key.replace("/", "\\").encode()).hexdigest() == hashed:
-                    return render_template("answer.html",
-                                           directories=self.result_list[key][0],
-                                           files=self.result_list[key][1],
-                                           current_dir=key,
-                                           previous_dir=previous_dir)
-            return "Directory not found"
 
         @lru_cache()
-        @app.route("/make_hash/")
-        @app.route("/make_hash/<path:next_dir>")
-        def make_hash(next_dir):
-            previous_dir = 0 if next_dir.replace("/", "\\") == os.environ["VIDEO"] \
-                else os.path.dirname(next_dir.replace("/", "\\"))
-            next_dir = hashlib.md5(next_dir.replace("/", "\\").encode()).hexdigest()
-            for key in self.result_list:
-                if hashlib.md5(key.replace("/", "\\").encode()).hexdigest() == next_dir:
+        @app.route("/<hashed>")
+        def answer(hashed):
+            result_list = self.get_file_tree()
+            hash_dict = {self.path_name.lower():
+                         hashlib.md5(self.path_name.replace("/", "\\").lower().encode()).hexdigest()}
+            for key in result_list:
+                hash_dict[key] = hashlib.md5(key.replace("/", "\\").lower().encode()).hexdigest()
+            for key in result_list:
+                previous_dir = 0 if key.replace("/", "\\") == os.environ["VIDEO"].lower() \
+                    else os.path.dirname(key.replace("/", "\\").lower())
+                if hashlib.md5(key.replace("/", "\\").lower().encode()).hexdigest() == hashed:
                     return render_template("answer.html",
-                                           directories=self.result_list[key][0],
-                                           files=self.result_list[key][1],
+                                           directories=result_list[key][0],
+                                           files=result_list[key][1],
                                            current_dir=key,
-                                           previous_dir=previous_dir)
+                                           previous_dir=previous_dir,
+                                           hash_dict=hash_dict)
             return "Directory not found"
+
         app.run()
+
+    def get_file_tree(self):
+        result_list = {}
+        for item in os.walk(self.path_name):
+            result_list[item[0].lower()] = ["\\" + x.lower() for x in item[1]], \
+                                           [y for y in item[2] if y[-4:] in self.file_extensions]
+        return result_list
 
 
 if __name__ == '__main__':
